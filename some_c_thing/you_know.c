4823ca9ee528ccb34b69f496c25989ae70e4493f
maulanazn <starknntnk99@gmail.com>
2022-11-30 19:00:56 +0700

    changing numerator fibonacci on first last number and first previous number

--- some_c_thing/you_know.c
+++ some_c_thing/you_know.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int fib(int n) {
+int fib_from_last_number(int n) {
    int last = 1;
    int prev = 0;
    int i;
@@ -18,6 +18,23 @@ int fib(int n) {
    return last;
 }
 
+int fib_from_previous_number(int n) {
+   int last = 1;
+   int prev = 0;
+   int i;
+
+   for (i = 0; i < n; ++i) {
+        int next = prev + last;
+
+        prev = last;
+        last = next;
+
+        printf("%d\n", next);
+   }
+
+   return prev;
+}
+
 int sequence(int n) {
     for (int i = n; i > 0; i--) {
         printf("%d\n", i);
@@ -26,7 +43,8 @@ int sequence(int n) {
 
 int main()
 {
-    fib(10);
+    fib_from_last_number(10);
+    fib_from_previous_number(10);
     sequence(10);
 
     return 0;
